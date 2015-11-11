957604eb016b9effe6d37710ffc44a83278f4f54
Joseph A. Szczesniak <NukaPunk@users.noreply.github.com>
2015-11-11 14:41:05 -0600

    Update group_01.js
    Added an `employees` array of arrays.

--- group_01.js
+++ group_01.js
@@ -1,4 +1,6 @@
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
+var atticus = ["Atticus", "2405", "47000", 3];
+var jem = ["Jem", "62347", "63500", 4];
+var boo = ["Boo", "11435", "54000", 3];
+var scout = ["Scout", "6243", "74750", 5];
+
+var employees = [atticus, jem, boo, scout];
