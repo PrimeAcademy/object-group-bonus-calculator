bb997e22659e91e4671b8ada2a5a936325e69cd7
Christopher Black <chris@primeacademy.io>
2018-03-01 15:53:26 -0600

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,10 @@
 const atticus = { name: 'Atticus', employeeNumber: '2405', annualSalary: '47000', reviewRating: 3 };
 const jem = { name: 'Jem', employeeNumber: '62347', annualSalary: '63500', reviewRating: 4 };
-const karen = { name: 'Karen', employeeNumber: '11435', annualSalary: '54000', reviewRating: 3 };
 const scout = { name: 'Scout', employeeNumber: '6243', annualSalary: '74750', reviewRating: 5 };
 const robert = { name: 'Robert', employeeNumber: '26835', annualSalary: '66000', reviewRating: 1 };
 const mayella = { name: 'Mayella', employeeNumber: '89068', annualSalary: '35000', reviewRating: 2 };
 
-const employees = [ atticus, jem, boo, scout, robert, mayella ];
+const employees = [ atticus, jem, scout, robert, mayella ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
