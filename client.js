53d69bcac0754d816dab80ccb87489daf4f4ca8b
Dev Jana <devjana@Devs-MacBook-Pro.local>
2018-05-15 15:44:36 -0500

    updated classes in js

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ class Employee{
 } // end Employee class
 
 const atticus = new Employee( 'Atticus', '2405', '47000', 3 );
-const jem = new Employee( 'Jem', '62347', '63500', 4 };
+const jem = new Employee( 'Jem', '62347', '63500', 4 );
 const scout =  new Employee( 'Scout', '6243', '74750', 5 );
 const robert =  new Employee( 'Robert', '26835', '66000', 1 );
 const mayella =  new Employee( 'Mayella', '89068', '35000', 2 );
