6785b0f01edbc232492870b41a1979d49ef604fd
Luke Schlangen <lukeschlangen@gmail.com>
2018-08-23 16:38:28 -0500

    add hints to client.js

--- client.js
+++ client.js
@@ -7,14 +7,21 @@ class Employee{
   } // end constructor
 } // end Employee class
 
-const atticus = new Employee( 'Atticus', '2405', '47000', 3 );
+const atticus = new Employee( 'Atticus', '2405', '47000', 3 ); // this creates a new object
 const jem = new Employee( 'Jem', '62347', '63500', 4 );
 const scout =  new Employee( 'Scout', '6243', '74750', 5 );
 const robert =  new Employee( 'Robert', '26835', '66000', 1 );
 const mayella =  new Employee( 'Mayella', '89068', '35000', 2 );
 
-const employees = [ atticus, jem, scout, robert, mayella ];
+const employees = [ atticus, jem, scout, robert, mayella ]; // this is an array of objects
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+
+// Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
+// This problem is massive! Break the problem down. Use the debugger.
+// What is the fewest lines of code I can write and test to get just a little closer?
+
+// This is not a race. Everyone on your team should understand what is happening.
+// Ask questions when you don't.
 
 console.log( employees );
