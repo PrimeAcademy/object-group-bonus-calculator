2f37fe04c4825341e08db51fbd3859fdac985b31
Luke Schlangen <lukeschlangen@gmail.com>
2019-01-28 16:48:14 -0600

    remove class constructor

--- client.js
+++ client.js
@@ -1,19 +1,35 @@
-class Employee{
-  constructor( name, employeeNumber, annualSalary, reviewRating ){
-    this.name = name;
-    this.employeeNumber = employeeNumber;
-    this.annualSalary = annualSalary;
-    this.reviewRating = reviewRating;
-  } // end constructor
-} // end Employee class
-
-const atticus = new Employee( 'Atticus', '2405', '47000', 3 ); // this creates a new object
-const jem = new Employee( 'Jem', '62347', '63500', 4 );
-const scout =  new Employee( 'Scout', '6243', '74750', 5 );
-const robert =  new Employee( 'Robert', '26835', '66000', 1 );
-const mayella =  new Employee( 'Mayella', '89068', '35000', 2 );
-
-const employees = [ atticus, jem, scout, robert, mayella ]; // this is an array of objects
+const employees = [
+  {
+    name: 'Atticus',
+    employeeNumber: '2405',
+    annualSalary: '47000',
+    reviewRating: 3
+  },
+  {
+    name: 'Jem',
+    employeeNumber: '62347',
+    annualSalary: '63500',
+    reviewRating: 4
+  },
+  {
+    name: 'Scout',
+    employeeNumber: '6243',
+    annualSalary: '74750',
+    reviewRating: 5
+  },
+  {
+    name: 'Robert',
+    employeeNumber: '26835',
+    annualSalary: '66000',
+    reviewRating: 1
+  },
+  {
+    name: 'Mayella',
+    employeeNumber: '89068',
+    annualSalary: '35000',
+    reviewRating: 1
+  }
+];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
