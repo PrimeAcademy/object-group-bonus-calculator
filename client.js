2ab841502cfc4f77b175d8a0d00ca8598ae682fc
Owner <owner@Owners-MacBook-Air.local>
2020-06-16 15:33:36 -0500

    Co-authored-by: Ace Fox <afox09@gmail.com> Co-authored-by: Carl Wilcoxon <carl@wilcoxon.org>

--- client.js
+++ client.js
@@ -1,34 +1,33 @@
-const employees = [
-  {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
-  },
-  {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
-  },
-  {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
-  },
-  {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
-  },
-  {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+const employees = [{
+        name: 'Atticus',
+        employeeNumber: '2405',
+        annualSalary: '47000',
+        reviewRating: 3
+    },
+    {
+        name: 'Jem',
+        employeeNumber: '62347',
+        annualSalary: '63500',
+        reviewRating: 4
+    },
+    {
+        name: 'Scout',
+        employeeNumber: '6243',
+        annualSalary: '74750',
+        reviewRating: 5
+    },
+    {
+        name: 'Robert',
+        employeeNumber: '26835',
+        annualSalary: '66000',
+        reviewRating: 1
+    },
+    {
+        name: 'Mayella',
+        employeeNumber: '89068',
+        annualSalary: '35000',
+        reviewRating: 1
+    }
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -40,4 +39,88 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+
+//Plan
+/* Loop over the `employees`
+array and do the following:
+    *
+    use each employee object as the input to the function
+described below.*`console.log`
+the results of each iteration.
+##Function Logic
+Write a declared
+function that takes in one ** Employee ** object(as an argument to the
+    function), and returns a new ** object ** with the following properties:
+*/
+let newArray = [];
+
+function bonusCalc(employees) {
+
+    for (let i = 0; i < employees.length; i++) {
+        console.log(eachCalculation(employees[i]));
+        newArray.push(eachCalculation(employees[i]));
+
+    } //end For loop
+    return newArray;
+}
+
+function eachCalculation(employee) {
+    let bonusObject = {};
+    if (employee.reviewRating <= 2) {
+        bonusObject.bonusPercentage = 0;
+    } else if (employee.reviewRating === 3) {
+        bonusObject.bonusPercentage = 4;
+    } else if (employee.reviewRating === 4) {
+        bonusObject.bonusPercentage = 6;
+    } else if (employee.reviewRating === 5) {
+        bonusObject.bonusPercentage = 10;
+    }
+
+    if (employee.name.length == 4) {
+        bonusObject.bonusPercentage += 5;
+    }
+
+    if (parseInt(employee.annualSalary) > 65000) {
+        bonusObject.bonusPercentage -= 1;
+    }
+
+    if (bonusObject.bonusPercentage < 0) {
+        bonusObject.bonusPercentage = 0;
+    }
+
+    if (bonusObject.bonusPercentage > 13) {
+        bonusObject.bonusPercentage = 13;
+    }
+
+    bonusObject.name = employee.name;
+    bonusObject.totalCompensation = employee.annualSalary * (1 + (bonusObject.bonusPercentage * .01));
+    bonusObject.totalBonus = parseInt(employee.annualSalary * (bonusObject.bonusPercentage * .01));
+
+    return bonusObject;
+
+}
+$(document).ready(calcButton);
+
+function calcButton() {
+    $("#calculatorButton").on('click', bonusCalc(employees));
+    let el = ("#employeeList");
+    for (i = 0; i < newArray; i++) {
+        el.append("<li> " + newArray[i] + " </li>");
+    }
+} //end calcButton
+
+
+
+/*
+let bonusObject = {
+    name: employeeName,
+    bonusPercentage: bonus,
+    totalCompensation: annualSalary * (1 + (bonus * .01)),
+    totalBonus: annualSalary * (bonus * .01)
+}
+
+let El = $( "#whateverTheIDIs");
+El.append("<li>" + WhateverWeWantToAppend + "</li>"")
+*/
